13d25efcc34ae214bd39f342b155f807287735ae
realeve@qq.com <realeve@qq.com>
2018-12-02 12:13:17 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  title: '数据接口开放平台',
+  description:
+    '整合SQLServer、MySql、ORACLE三大关系型数据库，抹平数据后端差异。',
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    repo: 'realeve/api_manager',
+    repoLabel: '查看源码',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    docsRepo: 'realeve/doc_api_manager',
+    // docsDir: '/',
+    // docsBranch: 'master',
+    editLinks: true,
+    nav: [
+      {
+        text: '手册',
+        link: '/guide/'
+      },
+      {
+        text: '进阶',
+        link: '/advance/'
+      },
+      {
+        text: '进入系统',
+        link: 'http://localhost:90/public/'
+      }
+    ],
+    search: false,
+    searchMaxSuggestions: 10,
+    sidebar: {
+      '/guide/': [
+        {
+          title: '操作手册',
+          collapsable: false,
+          children: ['', 'login', 'manual_check', 'abnormal_prod']
+        }
+      ]
+    }
+  }
+};
---
+++ README.md
@@ -0,0 +1,27 @@
+---
+home: true
+heroImage: /hero.svg
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+  - title: 接口鉴权
+    details: 基于JWT的接口鉴权，将用户身份状态与后端剥离，方便快速扩展，分布式部署。
+  - title: 数据缓存
+    details: 精准理解http协议，根据使用场景智能提供前后端数据缓存方案。
+  - title: 前端调用代码生成
+    details: 再也不用写繁琐的调用代码，前后端分离也可如此优雅。
+  - title: 满足各种应用场景
+    details: 系统提供RTX/Restful调用，JSON/XML，HTTP/Websocket多种应用场景。
+footer: © 2018 成都印钞有限公司 印钞管理部
+---
+
+```bash
+# install vuepress
+npm i -g vuepress
+
+# dev
+npm start
+
+# build
+npm build
+```
